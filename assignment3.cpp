5e533adbec4004375382e4590afe30a06c6f6a0b
Nathan Stephani <nathanstephani12@gmail.com>
2023-02-16 13:58:26 -0600

    main method to take input and run previous functions

--- assignment3.cpp
+++ assignment3.cpp
@@ -18,3 +18,20 @@ int ArrayProduct(vector<int> arr){
     }
     return product;
 }
+
+int main(){
+    vector<int> list = {};
+    int input;
+    while(cin >> input){ //takes unput until a non-integer is given
+        list.push_back(input); 
+    }
+    int sum = ArraySum(list);
+    cout << sum << endl;
+    int product = ArrayProduct(list);
+    cout << product << endl;
+    list = ReverseArray(list);
+    for(int i : list){
+        cout << i << " ";
+    }
+    return 0;
+}
